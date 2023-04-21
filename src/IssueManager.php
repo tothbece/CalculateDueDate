3c67d48ff21b2e15cf1db1ef460e35322265ab7c
Tóth Bence <tothbece@lauder.hu>
2023-04-21 18:23:56 +0200

    code cleanup & add some useful comments

--- src/IssueManager.php
+++ src/IssueManager.php
@@ -16,7 +16,7 @@ class IssueManager {
     {
         if ($turnaroundTime <= 0) throw new InvalidTurnaroundTimeException();
 
-        $startDayOfWeek = intval($startDatetime->format("N")); // 1 = Monday, 7 = Sunday
+        $startDayOfWeek = self::dayOfWeekFromTimestamp($startDatetime->getTimestamp());
         if ($startDayOfWeek >= 6) throw new NotWorkingHourException();
 
         $startTimeHour = intval($startDatetime->format("G")); // 0-23
@@ -27,13 +27,15 @@ class IssueManager {
         $endTimestamp = $startTimestamp;
 
         while ($turnaroundTimeInMinutes >= self::minutesUntilEndOfDay($startDatetime)) {
-            $endTimestamp += 24*60*60;
-            $turnaroundTimeInMinutes -= 8*60;
+            $endTimestamp += 24*60*60; // add 1 day
+            $turnaroundTimeInMinutes -= 8*60; // remove 8 hours of needed time
             // checking if we reached a weekend (Saturday)
-            if ( intval((new DateTime())->setTimestamp($endTimestamp)->format("N")) == 6 )
-                $endTimestamp += 2*24*60*60;
+            if ( self::dayOfWeekFromTimestamp($endTimestamp) == 6 )
+                $endTimestamp += 2*24*60*60; // shift to Monday
         }
-        $endTimestamp += $turnaroundTimeInMinutes * 60;
+        $endTimestamp += $turnaroundTimeInMinutes * 60; // adding remaining minutes
+        // NOTE: it could be negative if we jumped too further in time
+        //       when skipping not working hours
 
         return (new DateTime())->setTimestamp($endTimestamp);
 
@@ -47,6 +49,12 @@ class IssueManager {
         $endOfDay = new DateTime($date." 17:00");
         $deltaInSeconds = $endOfDay->getTimestamp()-$dateTime->getTimestamp();
         return intval($deltaInSeconds/60);
+    }
+
+
+    // 1 = Monday, 7 = Sunday
+    private static function dayOfWeekFromTimestamp(int $timestamp): int {
+        return intval((new DateTime())->setTimestamp($timestamp)->format("N"));
     }
 
 }
