da742d64ba98915805f1389dfd9a344bfc648c1e
Tóth Bence <tothbece@lauder.hu>
2023-04-21 17:15:23 +0200

    Should throw not working hours exception passes

--- src/IssueManager.php
+++ src/IssueManager.php
@@ -8,10 +8,20 @@ class IssueManager {
 
     /**
      * @throws InvalidTurnaroundTimeException
+     * @throws NotWorkingHourException
      */
     public static function calculateDueDate(DateTime $startDatetime, int $turnaroundTime): DateTime
     {
         if ($turnaroundTime <= 0) throw new InvalidTurnaroundTimeException();
+
+        $startDayOfWeek = intval($startDatetime->format("N")); // 1 = Monday, 7 = Sunday
+        if ($startDayOfWeek >= 6) throw new NotWorkingHourException();
+
+        $startTimeHour = intval($startDatetime->format("G")); // 0-23
+        if ($startTimeHour < 9 || $startTimeHour >= 17) throw new NotWorkingHourException();
+
         return new DateTime();
     }
+
+
 }
