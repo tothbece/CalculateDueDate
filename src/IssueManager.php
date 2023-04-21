4d69f29a2bed54238814038e2f027551ba33c2b6
Tóth Bence <tothbece@lauder.hu>
2023-04-21 17:04:02 +0200

    create calculateDueDate method & Should return date time object passes

--- src/IssueManager.php
+++ src/IssueManager.php
@@ -2,6 +2,12 @@
 
 namespace Emarsys\Issues;
 
+use DateTime;
+
 class IssueManager {
 
+    public static function calculateDueDate(DateTime $startDatetime, int $turnaroundTime): DateTime
+    {
+        return new DateTime();
+    }
 }
