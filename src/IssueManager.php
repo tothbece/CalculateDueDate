cba623a468193b0dcb8efaf518a30317cec6ba3b
Tóth Bence <tothbece@lauder.hu>
2023-04-21 17:06:09 +0200

    Should throw invalid turnaround time exception passes

--- src/IssueManager.php
+++ src/IssueManager.php
@@ -6,8 +6,12 @@ use DateTime;
 
 class IssueManager {
 
+    /**
+     * @throws InvalidTurnaroundTimeException
+     */
     public static function calculateDueDate(DateTime $startDatetime, int $turnaroundTime): DateTime
     {
+        if ($turnaroundTime <= 0) throw new InvalidTurnaroundTimeException();
         return new DateTime();
     }
 }
