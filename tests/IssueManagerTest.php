a14b691c289824e07b5c48e446cb06568b31830c
Tóth Bence <tothbece@lauder.hu>
2023-04-21 17:55:20 +0200

    add new test cases

--- tests/IssueManagerTest.php
+++ tests/IssueManagerTest.php
@@ -21,10 +21,12 @@ class IssueManagerTest extends TestCase {
     {
         return array(
           array(new DateTime("2023-04-17 10:15"), 2, new DateTime("2023-04-17 12:15")), // from Monday, 2h (same day ending)
+          array(new DateTime("2023-04-17 15:15"), 2, new DateTime("2023-04-18 09:15")), // from Monday, 2h (next day ending)
           array(new DateTime("2023-04-17 10:15"), 8, new DateTime("2023-04-18 10:15")), // from Monday, 8h (next day ending)
           array(new DateTime("2023-04-17 10:15"), 16, new DateTime("2023-04-19 10:15")), // from Monday, 16h (2 working days long issue)
           array(new DateTime("2023-04-20 10:15"), 16, new DateTime("2023-04-24 10:15")), // from Thursday, 16h (weekend)
           array(new DateTime("2023-04-20 10:15"), 56, new DateTime("2023-05-01 10:15")), // from Thursday, 56h (2 weekends)
+          array(new DateTime("2023-04-21 15:44"), 2, new DateTime("2023-04-24 09:44")), // from Friday, 2h (weekend in timespan)
         );
     }
 
