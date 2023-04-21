ed30849910e7ce8743d87483688d70bbcede44e9
Tóth Bence <tothbece@lauder.hu>
2023-04-21 17:30:35 +0200

    Should calculate within working hours with same day ending passes

--- src/IssueManager.php
+++ src/IssueManager.php
@@ -3,12 +3,14 @@
 namespace Emarsys\Issues;
 
 use DateTime;
+use Exception;
 
 class IssueManager {
 
     /**
      * @throws InvalidTurnaroundTimeException
      * @throws NotWorkingHourException
+     * @throws Exception
      */
     public static function calculateDueDate(DateTime $startDatetime, int $turnaroundTime): DateTime
     {
@@ -20,8 +22,23 @@ class IssueManager {
         $startTimeHour = intval($startDatetime->format("G")); // 0-23
         if ($startTimeHour < 9 || $startTimeHour >= 17) throw new NotWorkingHourException();
 
+        $startTimestamp = $startDatetime->getTimestamp();
+        $turnaroundTimeInMinutes = $turnaroundTime*60;
+        if ($turnaroundTimeInMinutes < self::minutesUntilEndOfDay($startDatetime)) {
+            return (new DateTime())->setTimestamp($startTimestamp+$turnaroundTimeInMinutes*60);
+        }
+
         return new DateTime();
     }
 
+    /**
+     * @throws Exception
+     */
+    private static function minutesUntilEndOfDay(DateTime $dateTime): int {
+        $date = $dateTime->format("Y-m-d");
+        $endOfDay = new DateTime($date." 16:00");
+        $deltaInSeconds = $endOfDay->getTimestamp()-$dateTime->getTimestamp();
+        return intval($deltaInSeconds/60);
+    }
 
 }
